3e28e1862ff7730647f34b22ad508992e569a815
intervalplace <julnerapple@hotmail.com>
2026-06-25 13:01:40 +0200

    Update fill.ts

--- src/validators/fill.ts
+++ src/validators/fill.ts
@@ -1,39 +1,28 @@
 import type { AonObject } from "../object.js";
 
 import {
-    requireObject,
     requireNamespace,
-    requireReferenceCount,
+    requireReferenceTypes,
 } from "./graph.js";
 
 export async function validateFill(
     obj: AonObject
 ) {
 
-    requireReferenceCount(obj, 4);
-
-    const makerAuth =
-        requireObject(
-            obj.references[0],
-            "authorization"
-        );
-
-    const takerAuth =
-        requireObject(
-            obj.references[1],
-            "authorization"
-        );
-
-    const makerOrder =
-        requireObject(
-            obj.references[2],
-            "order"
-        );
-
-    const takerOrder =
-        requireObject(
-            obj.references[3],
-            "order"
+    const [
+        makerAuth,
+        takerAuth,
+        makerOrder,
+        takerOrder,
+    ] =
+        requireReferenceTypes(
+            obj,
+            [
+                "authorization",
+                "authorization",
+                "order",
+                "order",
+            ]
         );
 
     requireNamespace(
