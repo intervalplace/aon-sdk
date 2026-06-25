83ee5f7c98d87c820ca0d7868695d9afcd68651c
intervalplace <julnerapple@hotmail.com>
2026-06-25 12:30:02 +0200

    Update index.ts

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,8 +1,35 @@
 import type { AonObject } from "../object.js";
-import { verifyObjectSignature } from "./signatures.js";
+
+import { validateAuthorization } from "./authorization.js";
+import { validateOrder } from "./order.js";
+import { validateRevocation } from "./revocation.js";
 
 export async function validateObject(
-  obj: AonObject
+    obj: AonObject
 ) {
-  await verifyObjectSignature(obj);
+
+    switch (obj.objectType) {
+
+        case "authorization":
+            await validateAuthorization(obj);
+            return;
+
+        case "order":
+            await validateOrder(obj);
+            return;
+
+        case "revocation":
+            await validateRevocation(obj);
+            return;
+
+        case "fill":
+        case "proof":
+        case "reserve":
+        case "receipt":
+        case "namespace_manifest":
+            return;
+
+        default:
+            throw new Error("UNKNOWN_OBJECT_TYPE");
+    }
 }
