ec9a9816967b0af8cffa870cb202c777b049d2c4
intervalplace <julnerapple@hotmail.com>
2026-06-25 12:57:37 +0200

    Update revocation.ts

--- src/validators/revocation.ts
+++ src/validators/revocation.ts
@@ -1,8 +1,15 @@
 import type { AonObject } from "../object.js";
+
 import { verifyObjectSignature } from "./signatures.js";
+import { requireObject } from "./graph.js";
 
 export async function validateRevocation(
     obj: AonObject
 ) {
+
     await verifyObjectSignature(obj);
+
+    requireObject(
+        obj.references[0]
+    );
 }
