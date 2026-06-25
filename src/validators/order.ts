0ecdc3c5ab8b8159aad7a4606ca1d71787665923
intervalplace <julnerapple@hotmail.com>
2026-06-25 12:56:25 +0200

    Update order.ts

--- src/validators/order.ts
+++ src/validators/order.ts
@@ -1,8 +1,28 @@
 import type { AonObject } from "../object.js";
 import { verifyObjectSignature } from "./signatures.js";
 
+import {
+    requireObject,
+    requireNamespace,
+    requireReferenceCount,
+} from "./graph.js";
+
 export async function validateOrder(
     obj: AonObject
 ) {
+
     await verifyObjectSignature(obj);
+
+    requireReferenceCount(obj, 1);
+
+    const auth =
+        requireObject(
+            obj.references[0],
+            "authorization"
+        );
+
+    requireNamespace(
+        auth.namespace,
+        obj.namespace
+    );
 }
