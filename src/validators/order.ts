a42b0e372da1f1e1db7c1624113b5dfa583ddb18
intervalplace <julnerapple@hotmail.com>
2026-06-28 20:36:47 +0200

    Update order.ts

--- src/validators/order.ts
+++ src/validators/order.ts
@@ -1,26 +1,22 @@
 import type { AonObject } from "../object.js";
-
 import { verifyObjectSignature } from "./signatures.js";
-
 import {
-    requireNamespace,
-    requireReferenceTypes,
+  requireNamespace,
+  requireReferenceTypes,
+  type ValidationGraph,
 } from "./graph.js";
 
-export async function validateOrder(
-    obj: AonObject
+export async function validateOrder(obj: AonObject) {
+  await verifyObjectSignature(obj);
+}
+
+export async function validateOrderGraph(
+  obj: AonObject,
+  graph: ValidationGraph
 ) {
+  await validateOrder(obj);
 
-    await verifyObjectSignature(obj);
+  const [auth] = requireReferenceTypes(graph, obj, ["authorization"]);
 
-    const [auth] =
-        requireReferenceTypes(
-            obj,
-            ["authorization"]
-        );
-
-    requireNamespace(
-        auth.namespace,
-        obj.namespace
-    );
+  requireNamespace(auth.namespace, obj.namespace);
 }
