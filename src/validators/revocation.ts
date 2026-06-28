cc8e344526c21ddd8ff5742caf09b24baba3adc5
intervalplace <julnerapple@hotmail.com>
2026-06-28 20:37:34 +0200

    Update revocation.ts

--- src/validators/revocation.ts
+++ src/validators/revocation.ts
@@ -1,19 +1,23 @@
 import type { AonObject } from "../object.js";
-
 import { verifyObjectSignature } from "./signatures.js";
-
 import {
-    requireReferenceTypes,
+  requireReferenceTypes,
+  type ValidationGraph,
 } from "./graph.js";
 
-export async function validateRevocation(
-    obj: AonObject
+export async function validateRevocation(obj: AonObject) {
+  await verifyObjectSignature(obj);
+
+  if ((obj.references ?? []).length !== 1) {
+    throw new Error("INVALID_REVOCATION_REFERENCE_COUNT");
+  }
+}
+
+export async function validateRevocationGraph(
+  obj: AonObject,
+  graph: ValidationGraph
 ) {
+  await validateRevocation(obj);
 
-    await verifyObjectSignature(obj);
-
-    requireReferenceTypes(
-        obj,
-        ["authorization"]
-    );
+  requireReferenceTypes(graph, obj, ["authorization"]);
 }
