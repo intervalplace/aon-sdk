354081bff893b401116dc50f756536948a825333
intervalplace <julnerapple@hotmail.com>
2026-06-28 20:37:00 +0200

    Update fill.ts

--- src/validators/fill.ts
+++ src/validators/fill.ts
@@ -1,47 +1,32 @@
 import type { AonObject } from "../object.js";
-
 import {
-    requireNamespace,
-    requireReferenceTypes,
+  requireNamespace,
+  requireReferenceTypes,
+  type ValidationGraph,
 } from "./graph.js";
 
-export async function validateFill(
-    obj: AonObject
+export async function validateFill(obj: AonObject) {
+  if ((obj.references ?? []).length !== 4) {
+    throw new Error("INVALID_FILL_REFERENCE_COUNT");
+  }
+}
+
+export async function validateFillGraph(
+  obj: AonObject,
+  graph: ValidationGraph
 ) {
+  await validateFill(obj);
 
-    const [
-        makerAuth,
-        takerAuth,
-        makerOrder,
-        takerOrder,
-    ] =
-        requireReferenceTypes(
-            obj,
-            [
-                "authorization",
-                "authorization",
-                "order",
-                "order",
-            ]
-        );
+  const [makerAuth, takerAuth, makerOrder, takerOrder] =
+    requireReferenceTypes(graph, obj, [
+      "authorization",
+      "authorization",
+      "order",
+      "order",
+    ]);
 
-    requireNamespace(
-        makerAuth.namespace,
-        obj.namespace
-    );
-
-    requireNamespace(
-        takerAuth.namespace,
-        obj.namespace
-    );
-
-    requireNamespace(
-        makerOrder.namespace,
-        obj.namespace
-    );
-
-    requireNamespace(
-        takerOrder.namespace,
-        obj.namespace
-    );
+  requireNamespace(makerAuth.namespace, obj.namespace);
+  requireNamespace(takerAuth.namespace, obj.namespace);
+  requireNamespace(makerOrder.namespace, obj.namespace);
+  requireNamespace(takerOrder.namespace, obj.namespace);
 }
