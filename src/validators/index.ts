09f69c9a769b7eb428938239c96023201901ed2a
intervalplace <julnerapple@hotmail.com>
2026-06-28 20:38:04 +0200

    Update index.ts

--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,50 +1,121 @@
 import type { AonObject } from "../object.js";
-import { validateReserve } from "./reserve.js";
-import { validateProof } from "./proof.js";
-import { validateFill } from "./fill.js";
-import { validateReceipt } from "./receipt.js";
-import { validateAuthorization } from "./authorization.js";
-import { validateOrder } from "./order.js";
-import { validateRevocation } from "./revocation.js";
+import type { ValidationGraph } from "./graph.js";
 
-export async function validateObject(
-    obj: AonObject
+import {
+  validateAuthorization,
+} from "./authorization.js";
+
+import {
+  validateOrder,
+  validateOrderGraph,
+} from "./order.js";
+
+import {
+  validateRevocation,
+  validateRevocationGraph,
+} from "./revocation.js";
+
+import {
+  validateReserve,
+  validateReserveGraph,
+} from "./reserve.js";
+
+import {
+  validateProof,
+  validateProofGraph,
+} from "./proof.js";
+
+import {
+  validateFill,
+  validateFillGraph,
+} from "./fill.js";
+
+import {
+  validateReceipt,
+  validateReceiptGraph,
+} from "./receipt.js";
+
+export async function validateObject(obj: AonObject) {
+  switch (obj.objectType) {
+    case "authorization":
+      await validateAuthorization(obj);
+      return;
+
+    case "order":
+      await validateOrder(obj);
+      return;
+
+    case "revocation":
+      await validateRevocation(obj);
+      return;
+
+    case "reserve":
+      await validateReserve(obj);
+      return;
+
+    case "proof":
+      await validateProof(obj);
+      return;
+
+    case "fill":
+      await validateFill(obj);
+      return;
+
+    case "receipt":
+      await validateReceipt(obj);
+      return;
+
+    case "namespace_manifest":
+      return;
+
+    default:
+      throw new Error("UNKNOWN_OBJECT_TYPE");
+  }
+}
+
+export async function validateObjectGraph(
+  obj: AonObject,
+  graph: ValidationGraph
 ) {
+  switch (obj.objectType) {
+    case "order":
+      await validateOrderGraph(obj, graph);
+      return;
 
-    switch (obj.objectType) {
+    case "revocation":
+      await validateRevocationGraph(obj, graph);
+      return;
 
-        case "authorization":
-            await validateAuthorization(obj);
-            return;
+    case "reserve":
+      await validateReserveGraph(obj, graph);
+      return;
 
-        case "order":
-            await validateOrder(obj);
-            return;
+    case "proof":
+      await validateProofGraph(obj, graph);
+      return;
 
-        case "revocation":
-            await validateRevocation(obj);
-            return;
+    case "fill":
+      await validateFillGraph(obj, graph);
+      return;
 
-case "reserve":
-    await validateReserve(obj);
-    return;
+    case "receipt":
+      await validateReceiptGraph(obj, graph);
+      return;
 
-case "proof":
-    await validateProof(obj);
-    return;
+    case "authorization":
+      await validateAuthorization(obj);
+      return;
 
-case "fill":
-    await validateFill(obj);
-    return;
+    case "namespace_manifest":
+      return;
 
-case "receipt":
-    await validateReceipt(obj);
-    return;
+    default:
+      throw new Error("UNKNOWN_OBJECT_TYPE");
+  }
+}
 
-case "namespace_manifest":
-    return;
-
-        default:
-            throw new Error("UNKNOWN_OBJECT_TYPE");
-    }
+export async function validateGraph(graph: ValidationGraph) {
+  for (const obj of graph.objects) {
+    await validateObjectGraph(obj, graph);
+  }
 }
